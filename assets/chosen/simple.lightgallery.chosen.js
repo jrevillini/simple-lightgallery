912c1c073c565fb72773d1990af1ec571d19a248
Savvas Hadjigeorgiou <savvasha@hotmail.com>
2021-10-04 08:28:26 +0300

    FIX: Set default chosen fields width

--- assets/chosen/simple.lightgallery.chosen.js
+++ assets/chosen/simple.lightgallery.chosen.js
@@ -1,5 +1,6 @@
 jQuery( document ).ready( function ( $ ) {
 
-	$( '#lightgallery_post_types' ).chosen( );
-	$( '#lightgallery_taxonomies' ).chosen( );
+	$( '#lightgallery_post_types' ).chosen( {width: "200px"} );
+	$( '#lightgallery_taxonomies' ).chosen( {width: "200px"} );
+	
 } );
